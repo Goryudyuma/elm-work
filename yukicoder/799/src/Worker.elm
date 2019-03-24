82f9265cb3dcfc23e7269117e63f6d93c6503227
Goryudyuma <umaretekyoumade@gmail.com>
2019-03-25 01:40:14 +0900

    yukicoder/799

---
+++ yukicoder/799/src/Worker.elm
@@ -0,0 +1,98 @@
+port module Worker exposing (main)
+
+{-| Example code for Qiita article
+-}
+
+import Json.Decode
+import Platform
+
+
+type alias Model =
+    { line : Int
+    , ans : String
+    }
+
+
+type Msg
+    = Input Int String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { line = 0, ans = "" }, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Input line message ->
+            let
+                list =
+                    String.split " " message
+                        |> List.map String.toInt
+                        |> List.map
+                            (\maybeValue ->
+                                case maybeValue of
+                                    Just value ->
+                                        value
+
+                                    Nothing ->
+                                        0
+                            )
+
+                a =
+                    case list |> List.drop 0 |> List.head of
+                        Just value ->
+                            value
+
+                        Nothing ->
+                            0
+
+                b =
+                    case list |> List.drop 1 |> List.head of
+                        Just value ->
+                            value
+
+                        Nothing ->
+                            0
+
+                c =
+                    case list |> List.drop 2 |> List.head of
+                        Just value ->
+                            value
+
+                        Nothing ->
+                            0
+
+                d =
+                    case list |> List.drop 3 |> List.head of
+                        Just value ->
+                            value
+
+                        Nothing ->
+                            0
+
+                ans =
+                    (b - a + 1) * (d - c + 1) - max 0 (min b d - max a c + 1)
+            in
+            ( { model | line = line + 1 }, cout <| String.fromInt ans )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    cin <| Input model.line
+
+
+port cout : String -> Cmd msg
+
+
+port cin : (String -> msg) -> Sub msg
+
+
+main : Platform.Program () Model Msg
+main =
+    Platform.worker
+        { init = \_ -> init
+        , update = update
+        , subscriptions = subscriptions
+        }
